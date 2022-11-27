46ac027e74f2f522e92f044ec2820b78d5a13820
Joao Vitor Guimaraes <joao.souza1@ufu.br>
2022-11-27 19:11:02 -0300

    att 3

---
+++ divisores.c
@@ -0,0 +1,23 @@
+/**
+* Arquivo: divisores.c
+* Autor: Joao Vitor Guimaraes de Souza
+* Matrícula: 12111EBI030
+* Criado em: 24/11/2022
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int n, inicio;
+
+    scanf("%d", &n);
+
+    for (inicio = 0; inicio <= n; inicio++) {
+        if (n % inicio == 0) {
+            printf("%d, ", inicio);
+        }
+    }
+    
+    return 0;
+}
